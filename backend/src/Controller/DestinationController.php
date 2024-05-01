223d22b2a554c2ad390faf75e973cd7791d6a5ef
feras <feras.altalib@gmail.com>
2024-05-01 22:24:08 +0200

    S

--- backend/src/Controller/DestinationController.php
+++ backend/src/Controller/DestinationController.php
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/destination')]
+#[Route('/destination' , name: 'app_destination_')]
 class DestinationController extends AbstractController
 {
-    #[Route('/', name: 'app_destination_index', methods: ['GET'])]
+    #[Route('s', name: 'index', methods: ['GET'])]
     public function index(DestinationRepository $destinationRepository): Response
     {
         return $this->render('destination/index.html.twig', [
@@ -22,7 +22,7 @@ class DestinationController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_destination_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $destination = new Destination();
@@ -42,7 +42,7 @@ class DestinationController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_destination_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Destination $destination): Response
     {
         return $this->render('destination/show.html.twig', [
@@ -50,7 +50,7 @@ class DestinationController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_destination_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Destination $destination, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(DestinationType::class, $destination);
@@ -68,7 +68,7 @@ class DestinationController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_destination_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Destination $destination, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$destination->getId(), $request->getPayload()->get('_token'))) {
