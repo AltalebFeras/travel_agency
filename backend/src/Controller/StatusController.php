bcca7483c26a78893ee1de815da0ede81c47bf53
feras <feras.altalib@gmail.com>
2024-05-05 20:24:42 +0200

    NEW HEADER

--- backend/src/Controller/StatusController.php
+++ backend/src/Controller/StatusController.php
@@ -34,6 +34,7 @@ class StatusController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($status);
             $entityManager->flush();
+            $this->addFlash('successS', 'The status has been added successfully');
 
             return $this->redirectToRoute('app_status_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -60,6 +61,7 @@ class StatusController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
+            $this->addFlash('successS', 'The status has been edited successfully');
 
             return $this->redirectToRoute('app_status_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -73,9 +75,18 @@ class StatusController extends AbstractController
     #[Route('/{id}', name: 'app_status_delete', methods: ['POST'])]
     public function delete(Request $request, Status $status, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$status->getId(), $request->getPayload()->get('_token'))) {
-            $entityManager->remove($status);
-            $entityManager->flush();
+        $submittedToken = $request->request->get('_token');
+
+        if ($this->isCsrfTokenValid('delete'.$status->getId(), $submittedToken)) {
+            try {
+                $entityManager->remove($status);
+                $entityManager->flush();
+                $this->addFlash('successS', 'The status has been deleted successfully');
+            } catch (\Exception $e) {
+                $this->addFlash('errorS', 'Unable to delete the status . It is associated with a valid reservation or client request.');
+            }
+        } else {
+            $this->addFlash('errorS', 'Invalid CSRF token.');
         }
 
         return $this->redirectToRoute('app_status_index', [], Response::HTTP_SEE_OTHER);
