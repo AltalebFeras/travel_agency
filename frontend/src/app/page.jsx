1096e63ed9f7b0b18b523da307733e0fed7a3f16
feras <feras.altalib@gmail.com>
2024-05-09 10:54:36 +0200

    front

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -11,6 +11,14 @@ export default function Home() {
     
     <Navbar/>
     <h1>Hello</h1>
+    <div className="grid grid-cols-3 p-4 gap-2">
+      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
+      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
+      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp" />
+      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
+      <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg" src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(78).webp" />
+    </div>
         </>
   );
 }
