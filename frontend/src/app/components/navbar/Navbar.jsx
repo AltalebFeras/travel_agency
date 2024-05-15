e43895753361bc491d742127c9dd6223203075b2
feras <feras.altalib@gmail.com>
2024-05-15 19:59:29 +0200

    MODIFICATIONS NAVBAR

--- frontend/src/app/components/navbar/Navbar.jsx
+++ frontend/src/app/components/navbar/Navbar.jsx
@@ -11,19 +11,37 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 function NavScrollExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="/">
-          <img src="https://i.ibb.co/y4X7N74/logo.png" alt="" />
+      <div>
+        <Navbar.Brand>
+          <img
+            className="logoImg"
+            src="https://i.ibb.co/y4X7N74/logo.png"
+            alt=""
+          />
         </Navbar.Brand>
-        <Navbar.Toggle  aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0 NavLinks" style={{ maxHeight: "130px" }} navbarScroll>
-            <Nav.Link className="btn btn-light" href="/">Home</Nav.Link>
-            <Nav.Link className="btn btn-light" href="/trip">Trips</Nav.Link>
-            <Nav.Link className="btn btn-light" href="/contact">Contact us</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+      </div>
+      <div >
+        <Container fluid className="containerFluid">
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0 NavLinks"
+              style={{ maxHeight: "130px" }}
+              navbarScroll
+            >
+              <Link className="btn btn-light" href="/">
+                Home
+              </Link>
+              <Link className="btn btn-light" href="/trip">
+                Trips
+              </Link>
+              <Link className="btn btn-light" href="/contact">
+                Contact us
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </div>
     </Navbar>
   );
 }
